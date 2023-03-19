1783c88dd9d95b5e1310f8e4b09ba00a796846ea
Newst <newstory7@yandex.ru>
2023-03-19 11:51:12 +0500

    hggg

--- AAA/infoPage/views.py
+++ AAA/infoPage/views.py
@@ -2,5 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 # Create your views here.
 def index(request):
-    question = get_object_or_404()
-    return render(request, 'polls/detail.html', {'question': question})
+    return render(request, 'infoPage/index.html')
