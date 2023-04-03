41127f44571cf60beb34f0566f410dd19c13b294
Newst7 <newstory7@yandex.ru>
2023-04-03 19:53:56 +0500

    mainpage complete

--- project_flask/app.py
+++ project_flask/app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/main")
 def hello_world():
     return render_template('index.html')
+
+@app.route("/reg")
+def hello_world():
+    return render_template('reg.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
